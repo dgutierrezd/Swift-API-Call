9d9027da0ef96285a7747cb8adc545bf5846e416
“Daniel <danielg@moxehub.com>
2022-04-14 18:18:02 -0500

    Initial Commit

---
+++ api-call/api_callApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_callApp.swift
+//  api-call
+//
+//  Created by Daniel Gutierrez on 14/04/22.
+//
+
+import SwiftUI
+
+@main
+struct api_callApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
